116fc8c11ebc1bd3120b18676f300714ac9f7e70
jt <j4c0bh3rrm4nn@gmail.com>
2019-03-01 23:31:19 -0700

    added game manager

---
+++ Assets/Prototype/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int turnCount;
+    public float turnTimeBaseSec;
+    public int SeasonIndicator; 
+
+
+
+    public int prPopulation;
+    public int prSterling;
+    public int prLumber;
+    public int prGrain;
+
+    public int scFarmstead;
+    public int scBuildAbilty;
+    public int scLumberCamp;
+    public int scStarvation;
+    public int scExposure;
+    public int scFireChance;
+
+    //buildingCount
+    int bcFarmstead = 0;
+    int bcCottage = 0;
+    int bcLumberCamp = 0;
+    int bcMarket = 0;
+    int bcForts = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
