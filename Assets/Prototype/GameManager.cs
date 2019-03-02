d01cdac73787da6f4722faaf4297ef3402967f1b
jt <j4c0bh3rrm4nn@gmail.com>
2019-03-02 09:57:42 -0700

    comit

--- Assets/Prototype/GameManager.cs
+++ Assets/Prototype/GameManager.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int turnCount;
-    public float turnTimeBaseSec;
-    public int SeasonIndicator; 
+    public int turnCount; // number of maxiumn turns
+    public float turnTimeBaseSec; // the number of seconds in a season
+    float NumberOfElapsedSeasonSec = 0; // the elapsed time in season  
+    public int SeasonIndicator; // 0 = spring, 1 summer 2 autumn 3 winter 
 
 
-
+    //player resources 
     public int prPopulation;
     public int prSterling;
     public int prLumber;
     public int prGrain;
 
-    public int scFarmstead;
-    public int scBuildAbilty;
-    public int scLumberCamp;
-    public int scStarvation;
-    public int scExposure;
-    public int scFireChance;
+    //scalers
+    
+    
+    int NumberBuildingsUnderConstruction; // currently under construction 
+
+    public int scFarmstead; //the bounus of grain for each farm
+    public int scLumberCamp;//the bounus of Lumber for each lubmer camp
+    public int scStarvation; // scaler used for determining how many people die from starvation
+    public int scExposure; // scaler used for determining how many people die from Exposuser
+    public int scFireChance; //base for fire chance 
+    int FireChance; 
 
     //buildingCount
     int bcFarmstead = 0;
