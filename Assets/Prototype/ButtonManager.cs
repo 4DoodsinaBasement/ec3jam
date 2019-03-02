7c24ac4de05472fa752c8a917ad90772c274fc08
nate.online <nate@travelwt.com>
2019-03-01 21:24:15 -0700

    prototype

---
+++ Assets/Prototype/ButtonManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Button farmButton;
+    public Button lumberButton;
+    public Button marketButton;
+    public Button houseButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectFarm()
+    {
+
+    }
+
+    void SelectLumber()
+    {
+
+    }
+
+    void SelectMarket()
+    {
+        
+    }
+
+    void SelectHouse()
+    {
+        
+    }
+}
