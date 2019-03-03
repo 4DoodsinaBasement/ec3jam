999ba33a07b616667dec297f9ccf8e4105b44c1c
nate.online <nate@travelwt.com>
2019-03-02 22:21:51 -0700

    added sprites and updated resource loader

--- Assets/Resources/ResourceLoader.cs
+++ Assets/Resources/ResourceLoader.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public static class ResourceLoader
 {
-    public static Sprite[] tile_field =  new Sprite[4];
-    public static Sprite[] tile_farm =  new Sprite[4];
+    public static Sprite[] spriteSheet_field =  new Sprite[4];
+    public static Sprite[] spriteSheet_cottage =  new Sprite[4];
+    public static Sprite[] spriteSheet_farm =  new Sprite[4];
+    public static Sprite[] spriteSheet_mill =  new Sprite[4];
+    public static Sprite[] spriteSheet_market =  new Sprite[4];
 
     static ResourceLoader()
     {
-        tile_field = Resources.LoadAll<Sprite>("Sprites/tile-field");
-        tile_farm = Resources.LoadAll<Sprite>("Sprites/tile-farm");
+        spriteSheet_field = Resources.LoadAll<Sprite>("Sprites/tile-field");
+        spriteSheet_cottage = Resources.LoadAll<Sprite>("Sprites/tile-cottage");
+        spriteSheet_farm = Resources.LoadAll<Sprite>("Sprites/tile-farm");
+        spriteSheet_mill = Resources.LoadAll<Sprite>("Sprites/tile-mill");
+        spriteSheet_market = Resources.LoadAll<Sprite>("Sprites/tile-market");
     }
 
     public static Sprite[] GetSpriteSheet(TileType tile)
@@ -18,11 +24,15 @@ public static class ResourceLoader
         switch (tile)
         {
             case TileType.Field :
-                return tile_field;
-                break;
+                return spriteSheet_field;
+            case TileType.Cottage :
+                return spriteSheet_cottage;
             case TileType.Farm :
-                return tile_farm;
-                break;
+                return spriteSheet_farm;
+            case TileType.Mill :
+                return spriteSheet_mill;
+            case TileType.Market :
+                return spriteSheet_market;
             default :
                 return null;
         }
