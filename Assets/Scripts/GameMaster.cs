046a0b85e2c35ca006b2e006d7ffdbbb1c33f430
Araliatrix <agentisaacson@gmail.com>
2019-03-03 03:54:32 -0700

    fixed typos

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -78,7 +78,7 @@ public class GameMaster : MonoBehaviour
     void Start()
     {
         currentSeason = seasonData[0];
-        notifyTray.AddNotification("Welcome to Ranoke!");
+        notifyTray.AddNotification("Welcome to Roanoke!");
         notifyTray.AddNotification(currentSeason.seasonString + " of year " + (ESTABLISH_YEAR + (currentTurns / 4)));
         
         LoadTiles();
